0d85987c8057c77c8a4f198bdbcd3a1516382288
aslaterm32 <aslaterm32@gmail.com>
2023-08-15 16:46:31 +0100

    working post routes

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -1,8 +1,9 @@
-const Post = require('../models/post')
+const Review = require('../models/post')
+const Token = require('../models/token')
 
 async function index(req, res) {
     try {
-        const posts = await Post.getAll()
+        const posts = await Review.getAll()
         res.status(200).json(posts)
     } catch (error) {
         res.status(500).json({ error: error.message })
@@ -12,7 +13,8 @@ async function index(req, res) {
 async function create(req, res) {
     try {
         const data = req.body
-        const result = await Post.create(data)
+        const token = await Token.getTokenByToken(req.headers['authorization'])
+        const result = await Review.create(data, token['user_id'])
         res.status(201).send(result)
     } catch (error) {
         res.status(400).json({ error: error.message })
@@ -22,7 +24,7 @@ async function create(req, res) {
 async function show(req, res) {
     try {
         const id = parseInt(req.params.id)
-        const result = await Post.getReviewById(id)
+        const result = await Review.getReviewById(id)
         res.status(200).json(result)
     } catch (error) {
         res.status(404).json({ error: error.message })
@@ -32,8 +34,8 @@ async function show(req, res) {
 async function destroy(req, res) {
     try {
         const id = parseInt(req.params.id)
-        const post = await Post.getReviewById(id)
-        const result = post.destroy()
+        const review = await Review.getReviewById(id)
+        const result = review.destroy()
         res.status(204).end()
     } catch (error) {}
 }
--- api/models/post.js
+++ api/models/post.js
@@ -1,40 +1,43 @@
-const db = require('../database/connect');
+const db = require('../database/connect')
 
 class Review {
-
-    constructor({ review_id, author, title, content}) {
-        this.id = review_id;
-        this.title = title;
-        this.author = author;
-        this.content = content;
+    constructor({ review_id, author, title, content }) {
+        this.id = review_id
+        this.title = title
+        this.author = author
+        this.content = content
     }
 
     static async getAll() {
-        const response = await db.query('SELECT * FROM reviews');
-        return response.rows.map(p => new post(p));
+        const response = await db.query('SELECT * FROM reviews')
+        return response.rows.map((p) => new Review(p))
     }
 
     static async getReviewById(id) {
-        const response = await db.query("SELECT * FROM reviews WHERE review_id = $1", [id]);
+        const response = await db.query('SELECT * FROM reviews WHERE review_id = $1', [id])
         if (response.rows.length != 1) {
-            throw new Error("Unable to locate post.")
+            throw new Error('Unable to locate post.')
         }
-        return new post(response.rows[0]);
+        return new Review(response.rows[0])
     }
 
-    static async create(data) {
-        const { author, title, content } = data;
-        let response = await db.query("INSERT INTO reviews (author, title, content) VALUES ($1, $2, $3) RETURNING review_id;",
-         [author, title, content]);
-        const newId = response.rows[0].post_id;
-        const newPost = await post.getReviewById(newId);
-        return newPost;
+    static async create(data, user_id) {
+        const { author, title, content } = data
+        let response = await db.query(
+            'INSERT INTO reviews (user_id, author, title, content) VALUES ($1, $2, $3, $4) RETURNING review_id;',
+            [user_id, author, title, content]
+        )
+        const newId = response.rows[0].review_id
+        const newPost = await Review.getReviewById(newId)
+        return newPost
     }
 
     async destroy() {
-        let response = await db.query("DELETE FROM reviews WHERE review_id = $1 RETURNING *;" [this.id]);
-        return new post(response.rows[0]);
+        let response = await db.query(
+            'DELETE FROM reviews WHERE review_id = $1 RETURNING *;'[this.id]
+        )
+        return new post(response.rows[0])
     }
 }
 
-module.exports = Review;
+module.exports = Review
