40d74d6868200c314f552429fdf6ffd8d0b1663e
aslaterm32 <aslaterm32@gmail.com>
2023-08-15 14:24:42 +0100

    cleanup

--- client/assets/register.js
+++ client/assets/register.js
@@ -11,16 +11,16 @@ document.getElementById('register-form').addEventListener('submit', async (event
             Accept: 'application/json',
             'Content-Type': 'application/json',
         },
-        body: {
+        body: JSON.stringify({
             username: form.get('username'),
             password: form.get('password'),
-        },
+        }),
     }
 
-    const response = await fetch('https://localhost:3000/users/register', options)
+    const response = await fetch('http://localhost:3000/users/register', options)
     const data = await response.json()
 
-    if (response.status === 201) {
+    if (response.status == 201) {
         window.location.assign('login.html')
     } else {
         const para = body.appendChild(document.createElement('p'))
